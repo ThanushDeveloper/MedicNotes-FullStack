b17982c93a0059220cac9cb2ceaf1846bb47e4b2
ThanushDeveloper <asthanush10@gmail.com>
2025-08-09 08:38:54 +0530

    Updated dashboard UI and fixed login bug

--- MedicNotes_Team-1/src/main/java/com/example/MedicNotes_Team_1/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.MedicNotes_Team_1.repository;
-
-import com.example.MedicNotes_Team_1.entity.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-    Admin findByEmail(String email);
-    Admin findByPhone(String phone);
-}
